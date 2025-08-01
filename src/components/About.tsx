03a5ce28b519b346cf1978f2f76a6487403477d6
Uros381 <urosjebrt@gmail.com>
2025-08-01 06:48:23 +0200

    Updated About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -26,7 +26,6 @@ const About: React.FC = () => {
   const stats = [
     { icon: Clock, value: '5+', label: 'Godina iskustva' },
     { icon: Users, value: '100+', label: 'Zadovoljnih klijenata' },
-    <div className="text-2xl">🚀</div>
     { icon: Award, value: '200+', label: 'Završenih projekata' },
     { icon: Palette, value: '∞', label: 'Kreativnih ideja' },
   ];
